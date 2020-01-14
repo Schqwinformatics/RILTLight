c88ef5ed71f5c69c0207050243cb21357018841e
Colin Simon <c.simon@progresso-group.de>
2020-01-14 12:28:57 +0100

    Add Network generator

---
+++ schlossNeuhausNetwork.py
@@ -0,0 +1,40 @@
+# the TestEnv environment is used to simply simulate the network
+from flow.envs import TestEnv
+
+# the Experiment class is used for running simulations
+from flow.core.experiment import Experiment
+
+# all other imports are standard
+from flow.core.params import VehicleParams
+from flow.core.params import NetParams
+from flow.core.params import InitialConfig
+from flow.core.params import EnvParams
+from flow.core.params import SumoParams
+from flow.networks import Network
+
+net_params = NetParams(
+    template={
+        "net": "sn.net.xml",
+        "rou": "sn.rou.xml"
+    }
+)
+
+env_params = EnvParams()
+sim_params = SumoParams(render=True)
+initial_config = InitialConfig()
+vehicles = VehicleParams()
+
+network = Network(
+    name='Schloß-Neuhaus',
+    net_params=net_params,
+    vehicles=vehicles
+)
+
+env = TestEnv(
+    env_params=env_params,
+    sim_params=sim_params,
+    network=network
+)
+
+exp = Experiment(env=env)
+exp.run(1, 1000)
